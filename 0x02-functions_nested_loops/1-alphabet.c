b3153b7e662e81abe08eb47468deb7424a0bfe54
Azeez Olurode <olurodeazeez@gmail.com>
2022-10-19 14:03:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main-print the alphabets in lower case
+ *
+ * Result: 1
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
